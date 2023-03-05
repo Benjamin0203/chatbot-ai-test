6e17d0e2a29c8f71ed8472f09af1d12118d5dc90
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-03-04 19:47:10 -0800

    --initial commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
+function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello I am chatGpt!",
+      sender: "ChatGpt",
+      direction: "outgoing",
+    },
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+    };
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+    //update messages state
+    //process message to chatGpt
+  };
+
+  return (
+    <div className="App">
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />;
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
