89f9da481bd857c1ee6a988b92c4a2fa9903fb7f
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-03-04 22:23:27 -0800

    --add chatGpt api

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,23 @@ import {
   TypingIndicator,
 } from "@chatscope/chat-ui-kit-react";
 
+// console.log("test: ", import.meta.env.VITE_API_KEY);
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+// console.log("API_KEY: ", API_KEY);
+
+const systemMessage = {
+  role: "system",
+  content: "Explain like Steve Jobs",
+};
+
 function App() {
+  const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([
     {
       message: "Hello I am chatGpt!",
       sender: "ChatGpt",
-      direction: "outgoing",
+      direction: "incoming",
     },
   ]);
 
@@ -24,19 +35,72 @@ function App() {
     const newMessage = {
       message: message,
       sender: "user",
+      direction: "outgoing",
     };
     const newMessages = [...messages, newMessage];
-    setMessages(newMessages);
+
     //update messages state
+    setMessages(newMessages);
+
+    //set a typing indicator(chatgpt is typing)
+    setTyping(true);
+
     //process message to chatGpt
+    await processMessageToChatGpt(newMessages);
   };
+  async function processMessageToChatGpt(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGpt") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message };
+    });
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setMessages([
+          ...chatMessages,
+          {
+            message: data.choices[0].message.content,
+            sender: "ChatGpt",
+          },
+        ]);
+        setTyping(false);
+      });
+  }
 
   return (
     <div className="App">
       <div style={{ position: "relative", height: "800px", width: "700px" }}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
+            <MessageList
+              scrollBehavior="smooth"
+              typingIndicator={
+                typing ? (
+                  <TypingIndicator content="chatGpt is typing..." />
+                ) : null
+              }
+            >
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />;
               })}
